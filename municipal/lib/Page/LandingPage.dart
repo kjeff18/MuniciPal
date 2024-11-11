012e1b68abbc955c0c54691cea74df7d428a98d8
dtaing11 <157168922+dtaing11@users.noreply.github.com>
2024-11-11 12:24:35 -0600

    Real Time Location Update
    Cutesy

--- municipal/lib/Page/LandingPage.dart
+++ municipal/lib/Page/LandingPage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:municipal/HelperFunction/UserLocation.dart';
 import 'package:municipal/widgets/CustomFloatingButton.dart';
 
 class LandingPage extends StatefulWidget {
@@ -11,44 +13,56 @@ class LandingPage extends StatefulWidget {
 
 class _LandingPageState extends State<LandingPage> {
   late GoogleMapController mapController;
+  UserLocation userLocation = UserLocation();
+  CameraPosition? _currentCameraPosition; // Change to a variable
 
- 
-  
-  void editAccountButtonFunc()
-  {
-
+  void editAccountButtonFunc() {
+  LatLng? currentPosition = userLocation.getCurrentPosition();
+     print('Currentsssss position: ${currentPosition!.latitude}, ${currentPosition!.longitude}');
   }
 
-  // Set initial camera position
-  static const CameraPosition _kInitialPosition = CameraPosition(
-    target:
-        LatLng(37.7749, -122.4194), // San Francisco's latitude and longitude
-    zoom: 12.0,
-  );
+  @override
+  void initState() {
+    super.initState();
+    userLocation.startLocationStream((LatLng position) {
+      setState(() {
+        _currentCameraPosition = CameraPosition(target: position, zoom: 15); // Update camera position
+      });
+      mapController.animateCamera(CameraUpdate.newLatLng(position));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-     final screenSize = MediaQuery.of(context).size;
+    final screenSize = MediaQuery.of(context).size;
     return Scaffold(
       body: Stack(
         children: [
-       
           GoogleMap(
-            initialCameraPosition: _kInitialPosition,
+            zoomControlsEnabled: true,
+            initialCameraPosition: _currentCameraPosition ?? const CameraPosition(
+              target: LatLng(37.7749, -122.4194), // Fallback to San Francisco initially
+              zoom: 15.0,
+            ),
             onMapCreated: (GoogleMapController controller) {
               mapController = controller;
             },
+            myLocationEnabled: true,
             myLocationButtonEnabled: false,
           ),
-
-            
           Positioned(
-           top: screenSize.height * 0.05 ,
-            right: screenSize.height * 0.02,
-            child: CustomFloatingButton(onPressed: editAccountButtonFunc))
-          
+            top: screenSize.height * 0.07,
+            right: screenSize.width * 0.02,
+            child: CustomFloatingButton(onPressed: editAccountButtonFunc),
+          ),
         ],
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    userLocation.dispose(); // Clean up the UserLocation instance
+    super.dispose();
   }
 }
