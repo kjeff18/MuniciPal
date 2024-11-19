0a31691d2a588a1076b114ccfc5e4b19ac93676c
dtaing11 <157168922+dtaing11@users.noreply.github.com>
2024-11-18 23:51:10 -0600

    Minor Bug Fixed

--- municipal/lib/Page/ReportPage.dart
+++ municipal/lib/Page/ReportPage.dart
@@ -86,13 +86,58 @@ class _ReportPageState extends State<ReportPage> {
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
               ReportHeader(iconPath: widget.iconPath, reportName: widget.reportName),
-              const SizedBox(height: 16),
-              DescriptionSection(controller: _controller),
-              const SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'Description',
+                    style: textFont.copyWith(
+                      fontSize: bodyTextSize,
+                      color: accentColor,
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  Text(
+                    '(${_controller.text.length}/200)',
+                    style: textFont.copyWith(
+                      fontSize: bodyTextSize,
+                      color: accentColor,
+                    ),
+                  ),
+                ],
+              ),
+             Padding(
+                padding: const EdgeInsets.only(bottom: defaultPadding),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: accentColor,
+                      width: 2,
+                    ),
+                    borderRadius: BorderRadius.circular(textFieldBorderRadius),
+                  ),
+                  width: double.infinity,
+                  height: 300, // Container height
+                  child: TextField(
+                    controller: _controller,
+                    maxLines: null,  // Allow infinite lines
+                    expands: true,    // Expands to take up all available space
+                    maxLength: 200,
+                    textAlignVertical: TextAlignVertical.top, // Align text to the top
+                    decoration: const InputDecoration(
+                      hintText: "Enter your text here...",
+                      border: InputBorder.none, // Remove the default border
+                      contentPadding: EdgeInsets.all(defaultPadding), // Padding inside the TextField
+                      counterText: "", // Hide the maxLength counter
+                    ),
+                    onChanged: (text) {
+                      setState(() {});
+                    },
+                  ),
+                ),
+              ),
               ImagePickerSection(onTap: _showCupertinoImagePicker),
-              Spacer(),
               CustomButton(text: "Submit", onPressed: SubmitButton, blueButton: true),
-              const SizedBox(height: 50)
             ],
           ),
         ),
