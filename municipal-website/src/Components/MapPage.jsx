609c92f797282aaba4b73044502de2fae7db836c
sahithi0504 <srampa1@lsu.edu>
2024-11-19 21:27:16 -0600

    added navbar

--- municipal-website/src/Components/MapPage.jsx
+++ municipal-website/src/Components/MapPage.jsx
@@ -1,28 +1,41 @@
-import React from 'react';
-import './MapPage.css';
+import React from "react";
+import "./MapPage.css";
 import Logo from "../assets/Logo.png";
-import Map from "../assets/Yalla-Abu-Dhabi-Map-2023.jpg";
-import GoogleMapComponent from './GoogleMapComponent';
-
-
-
+import TitleImage from "../assets/MuniciPalTitle.png";
+import { FaHome, FaProjectDiagram, FaUserCircle } from "react-icons/fa"; // Importing icons
+import GoogleMapComponent from "./GoogleMapComponent";
 
 const MapPage = () => {
-    return (
-        <div className="map-page">
-            <div className="map-placeholder">
-                <div className="logo-container">
-                    <img src={Logo} alt="MuniciPal Logo" className="logo" />
-                </div>
-                <div className="menu-container">
-                    <button className="menu-button">☰</button>
-                </div>
-                <div className="map-content">
-                    <GoogleMapComponent />
-                </div>
-            </div>
+  return (
+    <div className="map-page">
+      {/* Header containing logo and menu icons */}
+      <div className="header">
+        <div className="logo-container">
+          <img src={Logo} alt="MuniciPal Logo" className="logo" />
+          <img src={TitleImage} alt="Title" className="title-image" />
         </div>
-    );
+        <div className="menu-icons">
+          <button className="icon-button">
+            <FaHome className="icon" />
+            <span>Home</span>
+          </button>
+          <button className="icon-button">
+            <FaProjectDiagram className="icon" />
+            <span>My Projects</span>
+          </button>
+          <button className="icon-button">
+            <FaUserCircle className="icon" />
+            <span>Account</span>
+          </button>
+        </div>
+      </div>
+
+      {/* Map container */}
+      <div className="map-placeholder">
+        <GoogleMapComponent />
+      </div>
+    </div>
+  );
 };
 
 export default MapPage;
